4fc7b4e71ef6232348250ba7589d3d20684d1e7b
Adilson Quifucussa <kidikwenda@gmail.com>
2020-08-16 04:17:44 +0100

    v1 added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                deploy automatizado
+                deploy automatizado v1
             </div>
 
         </div>
